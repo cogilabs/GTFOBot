ef02fc8745ec7dc95c40c0984f76c84b45712507
0Davgi0 <0Davgi0@0DAVGI0-PC>
2022-12-10 22:18:40 +0100

    Added details to the /mission command

--- commands/mission.js
+++ commands/mission.js
@@ -19,18 +19,24 @@ module.exports = {
 				for(var lt in rundowns['R'+nb]){
 					for(var id in rundowns['R'+nb][lt]){
 						if(value == 'R' + nb + id){
-							response = '**Mission R' + nb + id + '**: *"' + rundowns['R'+nb][lt][id].name + '"*\n *Mission types:*\n'
+							response = '**Mission R' + nb + id + '**: *"' + rundowns['R'+nb][lt][id].name
+								+ '"*\n \n`:://Intel_`\n```' + rundowns['R'+nb][lt][id].intel + '```'
+								+ '\n`:://Sectors_&&_progression_permits_`\n'
 
 							for(var mt in rundowns['R'+nb][lt][id].missionTypes){
 								if(rundowns['R'+nb][lt][id].missionTypes[mt] == true){
 									response = response + ' - ' + mt + ': ';
 									if(rundowns['R'+nb][lt][id].completed[mt] == true){
-										response = response + '`✅` Completed\n';
+										response = response + '`✅ Completed`\n';
 									} else {
-										response = response + '`❌` Not completed\n';
+										response = response + '`❌ Not completed`\n';
 									}
 								}
 							}
+							response = response 
+							+ '\n`:://Interupted_Communications_`\n *' + rundowns['R'+nb][lt][id].description 
+							+ '*\n \n`:://Expedition_metrics_`\n' 
+							+ ' Drop cage target depth: `' + rundowns['R'+nb][lt][id].depth +'`m'
 						}
 					}
 				}
