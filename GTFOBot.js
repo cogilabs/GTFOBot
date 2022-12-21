2fbf8d0dc8eee7cd1eeb29a517cedec6c4b52683
0Davgi0 <0Davgi0@0DAVGI0-PC>
2022-12-21 12:21:53 +0100

    Added a reaction to @everyone messages + localized errors

--- GTFOBot.js
+++ GTFOBot.js
@@ -4,9 +4,9 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 const { supportedLocales } = require('./localization/supportedLocales.json');
 
-var loc = new Array();
+var locFile = new Array();
 for (var lang in supportedLocales) {
-    loc[lang] = require('./localization/' + lang + '.json');
+    locFile[lang] = require('./localization/' + lang + '.json');
 }
 
 const { rundowns } = require('./rundowns/rundowns.json');
@@ -15,7 +15,7 @@ global.rundowns = rundowns;
 const editJsonFile = require('edit-json-file');
 let file = editJsonFile('./rundowns/completion.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ] });
 global.client = client;
 
 client.commands = new Collection();
@@ -53,6 +53,20 @@ client.once(Events.ClientReady, () => {
 	client.user.setPresence({ activities: [{ name: 'GTFO' }], status: 'online' });
 });
 
+client.on(Events.MessageCreate, async message => {
+	if (message.author == client.user) return;
+
+	if (message.content.includes('@everyone')) {
+		var emojiName = 'DaudaPing'
+		const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === emojiName);
+		if (reactionEmoji != undefined) {
+			message.react(reactionEmoji);
+			console.log(`Reacted to “${message.content}” by ${message.author.tag}`)
+		} else console.log(`Tried tu react to “${message.content}” by ${message.author.tag} but no '${emojiName}' emoji was found`);
+	}
+
+});
+
 //Chat commands interactions
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
@@ -65,7 +79,12 @@ client.on(Events.InteractionCreate, async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        var locale = '';
+        for (var loc in supportedLocales) {
+            if (interaction.locale == loc) locale = interaction.locale;
+        }
+        if (locale == '') locale = 'en-US';
+		await interaction.reply({ content: locFile[locale][locale].system.commandError, ephemeral: true });
 	}
 });
 
@@ -83,7 +102,12 @@ client.on(Events.InteractionCreate, async interaction => {
 		await command.replyButton(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while treating the button!', ephemeral: true });
+        var locale = '';
+        for (var loc in supportedLocales) {
+            if (interaction.locale == loc) locale = interaction.locale;
+        }
+        if (locale == '') locale = 'en-US';
+		await interaction.reply({ content: locFile[locale][locale].system.buttonError, ephemeral: true });
 	}
 });
 
