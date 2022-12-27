0cb76e033f860211589e63fdf8679d75c37b2193
0Davgi0 <davgi.wifeo@gmail.com>
2022-12-27 22:36:27 +0100

    Localized /echo command

--- commands/echo.js
+++ commands/echo.js
@@ -1,23 +1,38 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { supportedLocales } = require('../localization/supportedLocales.json');
 const cmdName = 'echo';
 
-// TODO: Locale
+var locFile = new Array();
+for (var lang in supportedLocales) {
+    locFile[lang] = require('../localization/' + lang + '.json');
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName(cmdName)
-		.setDescription('Send a message in the current channel')
+		.setDescription(locFile['en-US']['en-US'].commands[cmdName].description)
+        .setDescriptionLocalizations({
+            fr: locFile['fr']['fr'].commands[cmdName].description,
+        })
 		.addStringOption(option => 
             option
             .setName('message')
-            .setDescription('The message to send in the current channel')
+			.setDescription(locFile['en-US']['en-US'].commands[cmdName].option1)
+			.setDescriptionLocalizations({
+				fr: locFile['fr']['fr'].commands[cmdName].option1,
+			})
 			.setRequired(true))
 		.setDefaultMemberPermissions(0)
 		.setDMPermission(false),
 	async execute(interaction) {
+        var locale = '';
+        for (var loc in supportedLocales) {
+            if (interaction.locale == loc) locale = interaction.locale;
+        }
+        if (locale == '') locale = 'en-US';
         var logsChannel = interaction.guild.channels.cache.find(channel => channel.name === logsChannelName);
 		const message = interaction.options.getString('message');
-		await interaction.reply({ content: 'Sending...', ephemeral: true });
+		await interaction.reply({ content: locFile[locale][locale].system.sending, ephemeral: true });
 		await interaction.channel.send({ content: message });
 		await interaction.deleteReply();
 		console.log(`@${interaction.user.tag} <@${interaction.user.id}> used “/${cmdName} ${message}”`);
