a259f5a8be24ea366ae6ea632db2f3877ef644f2
0Davgi0 <davgi.wifeo@gmail.com>
2023-01-16 15:26:52 +0100

    Added alert for user joining/leaving events

---
+++ delete-commands-global.js
@@ -0,0 +1,25 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, token } = require('./config.json');
+
+const commands = [];
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
